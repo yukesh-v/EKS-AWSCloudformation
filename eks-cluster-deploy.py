bc6ff54bcc6fb2c43eca5c36ec442a12d87b78a2
Yukesh <122459717+yukesh-v@users.noreply.github.com>
2026-05-15 20:33:57 +0530

    Fix

--- eks-cluster-deploy.py
+++ eks-cluster-deploy.py
@@ -40,7 +40,7 @@ def deploy_cluster(stack_name, region, subnet_ids, vpc_id, cluster_version, secu
                 print(f"\nDeployment Failed with status: {status}")
                 break
             else:
-                print(f"{stack_name} Cluster is being created...", end="\r", flush=True)
+                print(f"{stack_name} is being created...", end="\r", flush=True)
                 sys.stdout.flush()
                 time.sleep(30)
 
